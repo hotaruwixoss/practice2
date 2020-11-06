e6b4d2c6b3099cbc4b0adc94f72ddae3767b4335
hotaruwixoss <hotaruwixoss@gmail.com>
2020-11-05 21:56:36 -0400

    Add project files.

---
+++ practice2/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Linq;
+using System.Runtime.Intrinsics.X86;
+
+namespace practice2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            static double[] cargar(  )
+            {
+                 double salario;
+
+
+                Console.WriteLine("empleado 1: Mario Jimenez");
+                string empleado1 = Console.ReadLine();
+                double salario1 = 345000;
+
+                Console.WriteLine("empleado 1:  Neysha Alvarez" );
+                string empleado2 = Console.ReadLine();
+                double salario2 = 34000;
+
+                Console.WriteLine("empleado 1:  Neysha Alvarez" );
+                string empleado3 = Console.ReadLine();
+                double salario3 = 54000;
+
+                Console.WriteLine("empleado 1:  Neysha Alvarez" );
+                string empleado4 = Console.ReadLine();
+                double salario4 = 34000;
+            Console.WriteLine("empleado 1:  Marlo Roger");
+                string empleado5 = Console.ReadLine();
+                double salario5 = 5200;
+                return (cargar());
+
+            }
+
+
+            static double Salariomayor()
+            {
+
+                
+                Console.WriteLine("El empleado con el salario mayor es", cargar() );
+
+
+
+                return (Salariomayor());
+
+            }
+
+
+
+        }
+    }
+}
